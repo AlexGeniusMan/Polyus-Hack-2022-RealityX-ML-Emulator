162f2955a4fbdd8fd1e4b583141bff7e9674b0e0
Alexander Chentsov <alexgeniusman@gmail.com>
2022-10-15 15:18:24 +0400

    update

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from subprocess import Popen
 
 subprocesses = []
 
-for i in range(1):
+for i in range(15):
     subprocesses.append(Popen(
         ["venv/Scripts/python.exe", f"send.py", f"{i}"]
     ))
